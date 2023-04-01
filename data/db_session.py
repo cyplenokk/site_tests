7d21bcddbf0d8490cfc6c588adef87e8db6b5841
cyplenokk <ivanova.polina.ig@gmail.com>
2023-04-01 22:50:52 +0300

    with auto

--- data/db_session.py
+++ data/db_session.py
@@ -3,7 +3,6 @@ import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
 import sqlalchemy.ext.declarative as dec
 
-
 SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
@@ -27,6 +26,7 @@ def global_init(db_file):
     from . import __all_models
 
     SqlAlchemyBase.metadata.create_all(engine)
+
 
 def create_session() -> Session:
     global __factory
--- forms/user.py
+++ forms/user.py
@@ -1,12 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
+    name = StringField('Имя пользователя', validators=[DataRequired()])
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    name = StringField('Имя пользователя', validators=[DataRequired()])
-    about = TextAreaField("Почему вы выбрали данный сайт?")
+    submit = SubmitField('Войти')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Войти')
